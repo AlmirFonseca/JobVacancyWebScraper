4a2294709af9b7289d1520a15ac2d6baf2b2be04
Vinícius Sousa <vinicius.antunes@proton.me>
2023-12-10 09:37:58 -0300

    create emailer class test for email string validator

---
+++ tests/test_emailer.py
@@ -0,0 +1,24 @@
+import unittest
+from src.emailer.emailer import Emailer
+
+
+class TestEmailer(unittest.TestCase):
+    def test_is_valid_email_with_email(self):
+        email = "teste@gmail.com"
+        self.assertTrue(Emailer.is_valid_email(email))
+
+    def test_is_valid_email_no_dot_email(self):
+        email = "teste@gmailcom"
+        self.assertFalse(Emailer.is_valid_email(email))
+
+    def test_is_valid_email_no_at_sign_email(self):
+        email = "testegmail.com"
+        self.assertFalse(Emailer.is_valid_email(email))
+
+    def test_is_valid_email_with_plus_number(self):
+        email = "teste+1@gmail.com"
+        self.assertTrue(Emailer.is_valid_email(email))
+
+
+if __name__ == '__main__':
+    unittest.main()
