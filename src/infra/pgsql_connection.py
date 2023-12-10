62c17b6b734db6315e6cd59cffa256c79a26cd8b
MarianaLima2000 <marianalimab.2000@gmail.com>
2023-12-10 13:34:11 -0300

    adiciona classe de conexão com banco e arquivo config para centralizar envs

---
+++ src/infra/pgsql_connection.py
@@ -0,0 +1,38 @@
+import logging
+import traceback
+
+import psycopg2
+
+from config import ProductionConfig
+from src.infra.helper import Singleton
+
+logger = logging.getLogger(__name__)
+
+
+class PostgreSqlServerConnection(metaclass=Singleton):
+    def __init__(self, config=ProductionConfig):
+        self.server = config.server
+        self.port = config.port
+        self.database = config.database
+        self.username = config.username
+        self.password = config.password
+        self._connect()
+    
+    def _connect(self):
+        self.conn = psycopg2.connect(dbname=self.database, user=self.username,
+                                     password=self.password, host=self.server, port=self.port)
+
+
+    def __enter__(self):
+        logger.debug('Iniciando conexao com o Banco de Dados Stonelog',
+                     extra={'server': self.server,
+                            'port': self.port,
+                            'database': self.database,
+                            'username': self.username})
+        if not self.conn or self.conn.closed:
+            self._connect()
+        
+        return self.conn
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.conn.close()
