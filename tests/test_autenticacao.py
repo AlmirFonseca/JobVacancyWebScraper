6141e653aa566a49ff9d17f28a32981d82dee349
MarianaLima2000 <marianalimab.2000@gmail.com>
2023-12-10 20:13:12 -0300

    corrige import do teste

--- tests/test_autenticacao.py
+++ tests/test_autenticacao.py
@@ -1,18 +1,18 @@
 import unittest
 from src.user.user import User
-from src.user.user_dao import UserDao
+from src.user.user_dao import create
 
 
 class TestUsuarioAutenticado(unittest.TestCase):
     def test_is_connected(self):
         user = User('João', 'a', '123', '123', 'Silva')
-        a = UserDao.create(user, token="Teste")
+        a = create(user, token="Teste")
         self.assertIsNotNone(a)
 
 class TestUsuarioNaoAutenticado(unittest.TestCase):
     def test_is_connected(self):
         user = User('João', 'a', '123', '123', 'Silva')
         # Supondo que 'token' seja um argumento esperado pela função decorada.
-        self.assertRaises(ValueError, UserDao.create, user, token="NaoAutenticado")
+        self.assertRaises(ValueError, create, user, token="NaoAutenticado")
 
 
