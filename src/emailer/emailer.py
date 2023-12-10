11138648a457c488bf0b054923dad075d2c16e43
Vinícius Sousa <vinicius.antunes@proton.me>
2023-12-10 09:58:35 -0300

    implement is_valid_email TDD

--- src/emailer/emailer.py
+++ src/emailer/emailer.py
@@ -1,5 +1,21 @@
+import re
+
 class Emailer:
     def __init__(self): ...
 
     @staticmethod
-    def is_valid_email(email): ...
+    def is_valid_email(email: str) -> bool:
+        """
+                Checks if the provided email address is valid
+                Args:
+                    email(str): The email address
+                Returns:
+                    True if the provided email address is valid, False otherwise
+                """
+        # compilation error
+        # email_regex = r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$'
+        # doesn't pass test_is_valid_email_with_plus_number
+        # email_regex = r'^[a-zA-Z0-9-.]+@([\w-]+\.)+[\w-]{2,4}$'
+        email_regex = r'^[a-zA-Z0-9_.+-]+@([\w-]+\.)+[\w-]{2,4}$'
+
+        return bool(re.match(email_regex, email))
--- tests/test_emailer.py
+++ tests/test_emailer.py
@@ -19,6 +19,10 @@ class TestEmailer(unittest.TestCase):
         email = "teste+1@gmail.com"
         self.assertTrue(Emailer.is_valid_email(email))
 
+    def test_is_valid_email_with_underline(self):
+        email = "teste_t@gmail.com"
+        self.assertTrue(Emailer.is_valid_email(email))
+
 
 if __name__ == '__main__':
     unittest.main()
