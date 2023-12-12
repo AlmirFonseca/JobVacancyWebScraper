fd1b829b776ec80ac9602e40fbada6ae57c0fb34
MarianaLima2000 <marianalimab.2000@gmail.com>
2023-12-11 21:55:20 -0300

    melhora cobertura de testes da classe de user

--- tests/test_user.py
+++ tests/test_user.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append('./src/models')
 
 from user import User
+from daos.user_dao import *
 
 
 class TestUser(unittest.TestCase):
@@ -15,6 +16,16 @@ class TestUser(unittest.TestCase):
         key1 = User.generate_key('password1')
         key2 = User.generate_key('password2')
         self.assertNotEqual(key1, key2)
+    
+    def test_crud_user(self):
+        user_test = User('Sheldon', 'sheldon@gmail.com', 'password', 'token', 'Cooper', 1)
+        user_created = create(user_test)
+        test_email = get_by_email(user_test.email, token=user_test.token)
+        self.assertEqual(test_email.email, user_test.email)
+        test_id = get_by_id(user_created._id, token=user_test.token)
+        self.assertEqual(user_test.email, test_id.email)
+        user_deleted = delete(user_created._id, token=user_test.token)
+        self.assertIsNotNone(user_deleted)
 
 
 if __name__ == '__main__':
